b0ad66b215ad377cf40920279c14b1c19c4c53ea
Zoltan Farkas <progfarkas@gmail.com>
2025-10-30 23:56:24 +0100

    basic consumer pool

--- src/test/kotlin/com/example/cdc/CdcApplicationTests.kt
+++ src/test/kotlin/com/example/cdc/CdcApplicationTests.kt
@@ -2,7 +2,6 @@ package com.example.cdc
 
 import com.example.cdc.service.message.MessageRequestDto
 import com.example.cdc.service.message.MessageService
-import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.common.TopicPartition
 import org.assertj.core.api.Assertions.assertThat
@@ -17,6 +16,10 @@ import java.time.Instant
 import java.util.*
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.Executors
+import java.util.concurrent.Semaphore
+import java.util.concurrent.locks.Lock
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 
 @Import(TestcontainersConfiguration::class)
 @SpringBootTest
@@ -56,30 +59,49 @@ class CdcApplicationTests @Autowired constructor(
 	}
 
     private fun parallelRandomAccessIsSerial() {
-        val note = Vector<Pair<Instant, Long>>()
-        val executor = Executors.newVirtualThreadPerTaskExecutor()
-        val latch = CountDownLatch(10)
-        val props = Properties().apply {
-            this["bootstrap.servers"] = "${kafka.host}:${kafka.getMappedPort(9092)}"
-            this["enable.auto.commit"] = "false"
-            this["key.deserializer"] = "org.apache.kafka.common.serialization.StringDeserializer"
-            this["value.deserializer"] = "org.apache.kafka.common.serialization.StringDeserializer"
-            this["fetch.max.wait.ms"] = "100"
-            this["max.poll.records"] = "1"
-            this["default.api.timeout.ms"] = "3000"
+        fun createKafkaConsumer(): KafkaConsumer<String, String> {
+            val props = Properties().apply {
+                this["bootstrap.servers"] = "${kafka.host}:${kafka.getMappedPort(9092)}"
+                this["enable.auto.commit"] = "false"
+                this["key.deserializer"] = "org.apache.kafka.common.serialization.StringDeserializer"
+                this["value.deserializer"] = "org.apache.kafka.common.serialization.StringDeserializer"
+                this["fetch.max.wait.ms"] = "100"
+                this["max.poll.records"] = "1"
+                this["default.api.timeout.ms"] = "3000"
+            }
+            return KafkaConsumer<String, String>(props)
         }
-        KafkaConsumer<String, String>(props).use { consumer ->
-            val tp = TopicPartition("messages.public.messages", 0)
-            consumer.assign(listOf(tp))
-            repeat(10) {
-                executor.submit {
-                    randomAccess(consumer, tp, note)
+
+        val note = Vector<Pair<Instant, Long>>()
+        fun KafkaConsumer<String, String>.read(tp: TopicPartition, offset: Long) {
+            seek(tp, offset)
+            val records = poll(Duration.ofMillis(100))
+            note.add(Pair(Instant.now(), offset))
+            println("Partition ${tp.partition()} read: ${records.count()} records")
+        }
+
+        val pool = KafkaConsumerPool(::createKafkaConsumer, poolSize = 5)
+        val executor = Executors.newVirtualThreadPerTaskExecutor()
+        val latch = CountDownLatch(20)
+        repeat(20) {
+            executor.submit {
+                try {
+                    pool.useConsumer(TopicPartition("messages.public.messages", 0)) { consumer, tp ->
+                        consumer.read(tp, 0L)
+                        consumer.read(tp, 1L)
+                        consumer.read(tp, 2L)
+                        consumer.read(tp, 0L)
+                    }
+                } finally {
                     latch.countDown()
                 }
             }
-            latch.await()
         }
-        assertThat(note.size).isEqualTo(40)
+        latch.await()
+        pool.close()
+        executor.shutdown()
+
+        assertThat(note.size).isEqualTo(80)
         for (i in 0 until note.size - 1) {
             val current = note[i].first
             val next = note[i + 1].first
@@ -87,20 +109,68 @@ class CdcApplicationTests @Autowired constructor(
         }
     }
 
-    private fun randomAccess(consumer: KafkaConsumer<String, String>, tp: TopicPartition, note: Vector<Pair<Instant, Long>>) {
-        fun read(offset: Long): ConsumerRecord<String, String>? {
-            val records = synchronized(consumer) {
-                consumer.seek(tp, offset)
-                consumer.poll(Duration.ofMillis(100))
+}
+
+class KafkaConsumerPool<K, V>(
+    private val factory: () -> KafkaConsumer<K, V>,
+    private val poolSize: Int
+) {
+    private val consumers = mutableListOf<PooledConsumer<K, V>>()
+    private val assignments = mutableMapOf<TopicPartition, PooledConsumer<K, V>>()
+    private val lastUsed = LinkedHashMap<PooledConsumer<K, V>, Long>(poolSize, 0.75f, true)
+
+    private val lock = ReentrantLock(false)
+    private val semaphore = Semaphore(poolSize, true)
+
+    private data class PooledConsumer<K, V>(
+        val consumer: KafkaConsumer<K, V>,
+        val lock: Lock,
+    )
+
+    fun <R> useConsumer(
+        topicPartition: TopicPartition,
+        block: (KafkaConsumer<K, V>, TopicPartition) -> R
+    ): R {
+        semaphore.acquire()
+        try {
+            val pooled = lock.withLock {
+                val pooled = assignments[topicPartition]
+                    ?: consumers.firstOrNull { it !in assignments.values }
+                    ?: if (consumers.size < poolSize) {
+                        createPooledConsumer()
+                    } else {
+                        lastUsed.keys.first()
+                    }
+                lastUsed[pooled] = System.nanoTime()
+                pooled
             }
-            note.add(Pair(Instant.now(), offset))
-            return records.firstOrNull()
+
+            pooled.lock.withLock {
+                if (assignments[topicPartition] != pooled) {
+                    pooled.consumer.assign(listOf(topicPartition))
+                    assignments.entries.removeIf { it.value == pooled }
+                    assignments[topicPartition] = pooled
+                }
+                return block(pooled.consumer, topicPartition)
+            }
+        } finally {
+            semaphore.release()
         }
-        val records = listOf(read(0), read(1), read(2), read(0))
-        assertThat(records[0]?.offset()).isEqualTo(0)
-        assertThat(records[1]?.offset()).isEqualTo(1)
-        assertThat(records[2]?.offset()).isEqualTo(2)
-        assertThat(records[3]?.offset()).isEqualTo(0)
+    }
+
+    fun close() {
+        lock.withLock {
+            consumers.forEach { it.consumer.close() }
+            consumers.clear()
+        }
+    }
+
+    private fun createPooledConsumer(): PooledConsumer<K, V> {
+        val consumer = factory()
+        val pooledConsumer = PooledConsumer(consumer, ReentrantLock(true))
+        consumers += pooledConsumer
+        lastUsed[pooledConsumer] = System.nanoTime()
+        return pooledConsumer
     }
 
 }
