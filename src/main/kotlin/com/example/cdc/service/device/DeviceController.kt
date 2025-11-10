1cb079899483f0b7189a7ea9ef08c79e1904b0e1
Zoltan Farkas <progfarkas@gmail.com>
2025-11-10 23:44:31 +0100

    mongo dev

---
+++ src/main/kotlin/com/example/cdc/service/device/DeviceController.kt
@@ -0,0 +1,20 @@
+package com.example.cdc.service.device
+
+import com.example.cdc.repository.device.Device
+import com.example.cdc.repository.device.DeviceRepository
+import com.example.cdc.repository.device.Status
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
+import java.time.Instant
+
+@RestController
+class DeviceController(private val repository: DeviceRepository) {
+
+    @PostMapping("/devices")
+    fun createDevice(): Device {
+        val device = Device(name = "A", status = Status.ONLINE, lastSeen = Instant.now())
+        repository.save(device)
+        return device
+    }
+
+}
